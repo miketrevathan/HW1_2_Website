ef0b177c6ddcf054bc04634a034f060a67ce96e5
Mike Trevathan <trevathan.mike@gmail.com>
2019-09-20 04:13:56 -0400

    hw

---
+++ index.js
@@ -0,0 +1,56 @@
+function mitQuiz(){
+    
+var questions = [
+    {
+        prompt: "Where is MIT?\n(a) Texas \n(b) California \n(c) Massachusetts",
+        answers: "c"
+    },
+    {
+        prompt: "What is MIT's mascot?\n(a) Pilgrim \n(b) Beaver \n(c) Tiger",
+        answers: 'b'
+    },
+    {
+        prompt: "Who is the founder of MIT?\n(a) William Barton Rogers \n(b) Thomas Jeffereson \n(c) Charles Darwin",
+        answers: 'a'
+    }
+];
+
+var score = 0;
+
+for(var i=0; i < questions.length; i++){
+    var response = window.prompt(questions[i].prompt)
+    if(response == questions[i].answers){
+        score++;
+        alert("Correct!");
+    } else{
+        alert("WRONG!");
+    }
+}
+
+alert(" You Got" + score + " / " + questions.length);
+}
+
+function selVal(){
+   var selV = document.getElementById("selectid").value;
+    console.log(selV);
+    switch(selV){
+        case "MIT Classes":
+        document.getElementById("x").src="mitclass.jpg";
+        break;
+        case "Program":
+        document.getElementById("x").src="sdm.jpg";
+        break;
+        case "Company":
+        document.getElementById("x").src="chevron.jpg";
+        break;
+        case "Job":
+        document.getElementById("x").src="drillship.jpg";
+    }
+    }
+
+function mform(){
+    var FN = document.getElementById("fname").value;
+    var LN = document.getElementById("lname").value;
+    var EM = document.getElementById("email").value;
+    console.log(FN + LN+ EM);
+   }
